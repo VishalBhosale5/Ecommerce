3ef3d7496fd4ab88f443962b732b4d47959bbdf4
vishal <bhosalevishal510@gmail.com>
2019-03-04 16:19:50 +0530

    Added models template files

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('products_name','products_price','products_stock')
+
+admin.site.register(Product, ProductAdmin)
 # Register your models here.
--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    products_name = models.CharField(max_length=255)
+    products_price = models.FloatField()
+    products_stock = models.IntegerField()
+    products_urls= models.CharField(max_length=2055)
